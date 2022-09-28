708a5bb2b1d4d7863d191f0ce19e8e1dffecf98d
François Lavigne Marbach <developer@studiopixidream.com>
2022-09-28 02:48:43 +0200

    🎨 format(thierry): clean django generated code and add .env support

--- app/thierry/asgi.py
+++ app/thierry/asgi.py
@@ -2,15 +2,13 @@
 ASGI config for thierry project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
+# built in
 import os
 
+# third party
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thierry.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thierry.settings")
 application = get_asgi_application()
