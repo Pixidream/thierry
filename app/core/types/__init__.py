12fff52f1d78d15f0e281dc1c7b53f3e32030035
pixidream <developer@studiopixidream.com>
2022-10-20 16:30:44 +0200

    🏷️ types(youtube_data_api): add types for list video request

--- app/core/types/__init__.py
+++ app/core/types/__init__.py
@@ -1,4 +1,13 @@
 """core/types/__init__.py
 aggregate modules from types/
 """
-from .youtube_data_api import PlaylistInfos, PlaylistItem
+from .youtube_data_api import (
+    PlaylistInfos,
+    PlaylistItem,
+    VideoItem,
+    VideoStatus,
+    VideoSnippet,
+    VideoContentDetails,
+    VideoStatistics,
+    VideoResponse,
+)
--- app/core/types/youtube_data_api.py
+++ app/core/types/youtube_data_api.py
@@ -1,5 +1,5 @@
 """core/types/youtube_data_api.py
-handle types for youtube data api
+handle types for YouTube Data API
 """
 # built-in
 from typing import TypedDict, List, Dict
@@ -7,7 +7,7 @@ from typing import TypedDict, List, Dict
 
 class PlaylistItem(TypedDict):
     """
-    represent playlist item returned by youtube api
+    represent playlist item returned by YouTube Data API
     """
 
     contentDetails: Dict[str, str]
@@ -18,7 +18,7 @@ class PlaylistItem(TypedDict):
 
 class PlaylistInfos(TypedDict):
     """
-    represent playlist infos return by youtube
+    represent playlist infos return by YouTube Data API
     """
 
     etag: str
@@ -26,3 +26,91 @@ class PlaylistInfos(TypedDict):
     kind: str
     nextPageToken: str
     pageInfo: Dict[str, int]
+
+
+class VideoSnippet(TypedDict):
+    """
+    represent snippet part of VideoItem
+    """
+
+    publishedAt: str
+    channelId: str
+    title: str
+    description: str
+    thumbnails: Dict[str, str]
+    channelTitle: str
+    tags: List[str]
+    categoryId: str
+    liveBroadcastContent: str
+    defaultLanguage: str
+    localized: Dict[str, str]
+    defaultAudioLanguage: str
+
+
+class VideoContentDetails(TypedDict):
+    """
+    contentDetails part of VideoItem
+    """
+
+    duration: str
+    dimension: str
+    definition: str
+    caption: str
+    licensedContent: bool
+    regionRestriction: Dict[str, List[str]]
+
+
+class VideoStatus(TypedDict):
+    """
+    Status part of VideoItem
+    """
+
+    uploadStatus: str
+    failureReason: str
+    rejectionReason: str
+    privacyStatus: str
+    publishAt: str
+    license: str
+    embeddable: bool
+    publicStatsViewable: bool
+    madeForKids: bool
+    selfDeclaredMadeForKids: bool
+
+
+class VideoStatistics(TypedDict):
+    """
+    stat part of VideoItem
+    """
+
+    viewCount: str
+    likeCount: str
+    dislikeCount: str
+    favoriteCount: str
+    commentCount: str
+
+
+class VideoItem(TypedDict):
+    """
+    represent video item return by YouTube Data API
+    """
+
+    kind: str
+    etag: str
+    id: str
+    snippet: VideoSnippet
+    contentDetails: VideoContentDetails
+    status: VideoStatus
+    statistics: VideoStatistics
+
+
+class VideoResponse(TypedDict):
+    """
+    represent the response from YouTube Data Api when listing videos
+    """
+
+    kind: str
+    etag: str
+    nextPageToken: str
+    prevPageToken: str
+    pageInfo: Dict[str, int]
+    items: List[VideoItem]
