2cc94e3b9479c52fa259dae61f8cd0849ee2f6d6
François Lavigne Marbach <developer@studiopixidream.com>
2022-10-20 02:58:35 +0200

    ✅ test(core): test youtube_data_api.get_infos method

---
+++ app/tests/core/test_youtube_data_api.py
@@ -0,0 +1,31 @@
+"""tests/core/test_youtube_data_api.py
+unittest methods from app/core/utils/youtube_data_api.py
+"""
+# built in
+from unittest import TestCase
+from os import getenv
+
+# project modules
+from core.utils import YoutubeDataApi
+
+
+class YoutubeDataApiTest(TestCase):
+    """
+    class that handle all tests
+    """
+
+    def setUp(self) -> None:
+        """
+        Method called to prepare the test fixture
+        """
+        self.yt_api = YoutubeDataApi()
+
+    def test_get_infos(self):
+        """
+        run the get_infos method and check that etag and kind are valid
+        and then that there is a least on item returned
+        """
+        infos = self.yt_api.get_infos(getenv("DEBRIEF_ACTU_PLAYLIST_ID"))
+        self.assertEqual(infos["etag"], "Mh6ctf1-xZT_rAeX2iLHOPhKs6w")
+        self.assertEqual(infos["kind"], "youtube#playlistItemListResponse")
+        self.assertTrue(len(infos["items"]) > 0)
