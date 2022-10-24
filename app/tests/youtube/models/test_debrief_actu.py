790bafaf69d31c12728ad699f834bd7998873308
pixidream <developer@studiopixidream.com>
2022-10-24 22:22:41 +0200

    🧪 test(youtube-model): test get DebriefActu from database

--- app/tests/youtube/models/test_debrief_actu.py
+++ app/tests/youtube/models/test_debrief_actu.py
@@ -3,6 +3,7 @@ verify that i can create a debrief actu objects
 """
 # built-in
 from datetime import date
+from typing import Dict
 
 # third-party
 from django.test import TestCase
@@ -23,18 +24,32 @@ class TestDebriefActu(TestCase):
             "release_date": str(date.today()),
             "thumbnail": "https://i.ytimg.com/vi/yol-kVahVWQ/hqdefault.jpg",
         }
+        DebriefActu.objects.create(
+            vid="toto", title="toto", release_date="12-12-2022", thumbnail="https://google.com"
+        )
 
-        self.debrief = None
+    def _assert_debrief(self, debrief: DebriefActu, params: Dict[str, str]):
+        self.assertIsInstance(debrief, DebriefActu)
+        self.assertEqual(params["vid"], debrief.vid)
+        self.assertEqual(params["title"], debrief.title)
+        self.assertEqual(params["release_date"], debrief.release_date)
+        self.assertEqual(params["thumbnail"], debrief.thumbnail)
 
     def test_01_create_debrief(self):
         """
         check that DebriefActu can be registered to database
         """
-        debrief = DebriefActu(**self.dummy)
-        debrief.save()
+        debrief = DebriefActu.objects.create(**self.dummy)
+        self._assert_debrief(debrief, self.dummy)
 
-        self.assertIsInstance(debrief, DebriefActu)
-        self.assertEqual(self.dummy["vid"], debrief.vid)
-        self.assertEqual(self.dummy["title"], debrief.title)
-        self.assertEqual(self.dummy["release_date"], debrief.release_date)
-        self.assertEqual(self.dummy["thumbnail"], debrief.thumbnail)
+    def test_02_get_debrief(self):
+        """
+        check that we can get a debrief from database
+        """
+        debrief = DebriefActu.objects.get(vid="toto")
+        self._assert_debrief(
+            debrief,
+            dict(
+                vid="toto", title="toto", release_date="12-12-2022", thumbnail="https://google.com"
+            ),
+        )
