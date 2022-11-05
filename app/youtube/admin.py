d0aa893fd6799d08dba813b63d3dcf85a69d3461
François Lavigne Marbach <developer@studiopixidream.com>
2022-11-05 20:39:22 +0100

    ✨ feat(youtube_administration): add Title, Tag, DebriefActu models to admin

--- app/youtube/admin.py
+++ app/youtube/admin.py
@@ -1,2 +1,13 @@
 """youtube/admin.py
+manage administration panel for youtube app
 """
+# third party
+from django.contrib import admin
+
+# project modules
+from youtube.models import Title, Tag, DebriefActu
+
+
+admin.site.register(Title)
+admin.site.register(Tag)
+admin.site.register(DebriefActu)
