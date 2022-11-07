c4951d113538871a3c210aaf000d6ee77cf2459d
François Lavigne Marbach <developer@studiopixidream.com>
2022-11-07 22:41:04 +0100

    ✨  feat(youtube-managment): add updatedebriefs command
    closes #29

--- app/youtube/management/commands/updatedebriefs.py
+++ app/youtube/management/commands/updatedebriefs.py
@@ -8,7 +8,9 @@ from typing import Any
 from django.core.management.base import BaseCommand
 
 # project package
-# from core.utils import YoutubeDataApi
+from core.utils import YoutubeDataApi
+
+from youtube.models import Tag as TagModel, DebriefActu as DebriefActuModel, Title as TitleModel
 
 
 class Command(BaseCommand):
@@ -22,5 +24,38 @@ class Command(BaseCommand):
         """
         load the youtube data api and fetch videos id from Debrief actu playlist
         """
-        # yt_api = YoutubeDataApi()
-        return
+        yt_api = YoutubeDataApi()
+
+        debriefs = yt_api.serialize_debriefs_from_api()
+        counter = dict(debrief=0, tag=0, title=0, error=0)
+
+        for debrief in debriefs:
+            tags = debrief["tags"]
+            titles = debrief["titles"]
+
+            debrief.pop("tags")
+            debrief.pop("titles")
+
+            try:
+                debrief["release_date"] = str(debrief["release_date"]).split("T", maxsplit=1)[0]
+                created_debrief = DebriefActuModel.objects.create(**debrief)
+                counter["debrief"] += 1
+
+                for tag in tags:
+                    TagModel.objects.create(**tag, video=created_debrief)
+                    counter["tag"] += 1
+
+                for title in titles:
+                    TitleModel.objects.create(**title, video=created_debrief)
+                    counter["title"] += 1
+
+            except Exception as error:  # pylint: disable=broad-except
+                self.stderr.write(self.style.ERROR(error))
+                counter["error"] += 1
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"{counter['debrief']} debriefs, {counter['title']} titles,"
+                f"{counter['tag']} tags synced and {counter['error']} failed"
+            )
+        )
